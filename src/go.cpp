e8fe8bce7c362de82d2792d9935c5cc500fb718f
Lincoln97 <lincolnboer@gmail.com>
2016-03-21 17:40:37 -0300

    fiveth

--- src/go.cpp
+++ src/go.cpp
@@ -5,12 +5,20 @@
 #include <geometry_msgs/Twist.h>
 #include <math.h>
 
-#define xf 4
-#define yf 1.5
-
 geometry_msgs::Twist v;
 
 ros::Publisher v_pub;
+
+double normalizeAngle(double angle)
+{
+    if(angle > M_PI){
+        return(angle - 2*M_PI);
+    }else if(angle < -M_PI){
+        return (angle + 2*M_PI);
+    }else{
+        return angle;
+    }
+}
 
 void odomCallback(const nav_msgs::OdometryConstPtr &msg){
     
@@ -18,38 +26,41 @@ void odomCallback(const nav_msgs::OdometryConstPtr &msg){
     
     qt = msg->pose.pose.orientation; 
     
-    double yaw = tf::getYaw(qt);
+    double yaw = tf::getYaw(qt)*180/M_PI;
     
-    std::cout << "Yaw: " << yaw*180/M_PI << std::endl;
+    std::cout << "Yaw: " << yaw << std::endl;
     
-    double x = 0, y = 0;
+    double x = 0, y = 0, xf = -3, yf = 1.5, t;
     x = msg->pose.pose.position.x;
     y = msg->pose.pose.position.y;
     
-    double d = sqrt(((x - xf)*(x - xf)) + ((y - yf)*(y - yf)));
+    double d = sqrt(((xf - x)*(xf - x)) + ((yf - y)*(yf - y)));
     
     std::cout << "d: " << d << std::endl;
     
-    double t = atan((yf - y)/(xf - x));
+    t = atan2((yf - y),(xf - x))*180/M_PI;
+        
+    std::cout << "t: " << t << std::endl;
     
-    double dt = (t - yaw)*180/M_PI;
+    double dt = normalizeAngle(t - yaw);
+    
+    std::cout << "dt: " << dt << std::endl;
     
     if(d > 0.1){
         if(dt > 0.5){
             v.linear.x = 2;
             v.angular.z = 0.1*dt;
         }
-        else{
-            if(dt < -0.5){
-                v.linear.x = 2;
-                v.angular.z = 0.1*dt;
-            }
-            else {
-                v.linear.x = 2;
-                v.angular.z = 0;
-            }
+        else if(dt < -0.5){
+            v.linear.x = 2;
+            v.angular.z = 0.1*dt;
+        }
+        else {
+            v.linear.x = 2;
+            v.angular.z = 0;
         }
     }
+    
     if(d < 0.1){
         v.linear.x = 0;
         v.angular.z = 0;
